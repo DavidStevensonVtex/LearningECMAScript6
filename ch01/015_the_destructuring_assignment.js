0db856c04432d3c16e209d264190d9332627bab4
David Stevenson <drstevenson1958@yahoo.com>
2023-07-01 12:40:39 -0400

    The array destructuring assignment

---
+++ ch01/015_the_destructuring_assignment.js
@@ -0,0 +1,8 @@
+// The array destructuring assignment
+
+let myArray = [ 1, 2, 3 ] ;
+let a, b, c ;
+
+[ a, b, c ] = myArray ;     // Array destructuring assignment syntax
+
+console.log("a = ", a, " b = ", b, " c = ", c );    // a =  1  b =  2  c =  3
