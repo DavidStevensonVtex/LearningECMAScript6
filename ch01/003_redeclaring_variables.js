f3f932587236e116ebba1e7953f7fe0fb305f94a
David Stevenson <drstevenson1958@yahoo.com>
2023-07-01 11:39:19 -0400

    Re-declaring variables

---
+++ ch01/003_redeclaring_variables.js
@@ -0,0 +1,17 @@
+var a = 0 ;
+var a = 1 ;
+
+console.log("a = ", a);
+
+function myFunction()
+{
+    var b = 2 ;
+    var b = 3 ;
+
+    console.log("b = ", b);
+}
+
+myFunction();
+
+// a =  1
+// b =  3
