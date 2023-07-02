924b2fef9ff7132f7446ce341927679f2e01d4cd
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 13:19:57 -0400

    The octal notation

---
+++ ch02/002_the_octal_notation.js
@@ -0,0 +1,9 @@
+// var a = 017 ;   // ECMAScript 5 // Octal literals are not allowed. Use the syntax '0o17'.
+var a = 0o17 ;
+var b = 15 ;
+
+console.log("a = ", a, " b = ", b );
+console.log("a == b ", a == b) ;
+
+// a =  15  b =  15
+// a == b  true
