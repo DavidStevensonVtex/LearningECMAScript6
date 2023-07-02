9f7dee6695a2f473b9ebfe67a9651708ffe7b3f3
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:11:17 -0400

    The Math.clz32(number) function

---
+++ ch02/011_the_Math_clz32_number_function.js
@@ -0,0 +1,9 @@
+// The Math.clz32(32) function returns the number of leading zero bits in the 32-bit number.
+
+console.log("Math.clz32(7))          ", Math.clz32(7));
+console.log("Math.clz32(1_000)       ", Math.clz32(1_000));
+console.log("Math.clz32(295_000_000))", Math.clz32(295_000_000));
+
+// Math.clz32(7))           29
+// Math.clz32(1_000)        22
+// Math.clz32(295_000_000)) 3
