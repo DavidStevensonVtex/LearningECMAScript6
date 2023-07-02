37cc03489e12a31cdecee13559931f1586362857
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:16:51 -0400

    The Math.trunc(number) function

---
+++ ch02/013_the_Math_trunc_number_function.js
@@ -0,0 +1,7 @@
+// The Math.trunc function removes any fractional portion of the number leaving an integer.
+
+console.log("Math.trunc(11.17)  ", Math.trunc(11.17));
+console.log("Math.trunc(-1.112) ", Math.trunc(-1.112));
+
+// Math.trunc(11.17)   11
+// Math.trunc(-1.112)  -1
