4d51601fed85a14d09ae5bd4db432103eef97e57
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 13:59:37 -0400

    Arithmetic functions

---
+++ ch02/009_arithmetic_functions.js
@@ -0,0 +1,11 @@
+console.log("Math.log2(16)    ", Math.log2(16));        // log base 2
+console.log("Math.log10(1000))", Math.log10(1000));     // log base 10
+console.log("Math.log1p(0))   ", Math.log1p(0));        // same as log(1 + value)
+console.log("Math.expm1(0))   ", Math.expm1(0));        // inverse of Math.log1p()
+console.log("Math.cbrt(8)     ", Math.cbrt(8));         // cube root of a value
+
+// Math.log2(16)     4
+// Math.log10(1000)) 3
+// Math.log1p(0))    0
+// Math.expm1(0))    0
+// Math.cbrt(8)      2
