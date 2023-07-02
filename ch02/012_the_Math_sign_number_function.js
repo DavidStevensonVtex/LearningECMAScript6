4334b8f1e06d6838136cb0d51e302b6a5c4e91fb
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:14:16 -0400

    The Math.sign(number) function

---
+++ ch02/012_the_Math_sign_number_function.js
@@ -0,0 +1,7 @@
+console.log("Math.sign(11) ", Math.sign(11));
+console.log("Math.sign(-11)", Math.sign(-11));
+console.log("Math.sign(0)  ", Math.sign(0));
+
+// Math.sign(11)  1
+// Math.sign(-11) -1
+// Math.sign(0)   0
