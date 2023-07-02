8ada3a6dc669a6f2c3dd62b2f437d16a86eef9b9
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 12:08:57 -0400

    The Arrow Functions - 2

---
+++ ch01/030_the_arrow_functions_2.js
@@ -0,0 +1,6 @@
+let circleArea = (pi, r) => pi * r * r ;
+
+let result = circleArea(3.14, 3) ;
+
+console.log(`Area of circle with radius of: 3: ${result.toFixed(2)}`);
+// Area of circle with radius of: 3: 28.26
