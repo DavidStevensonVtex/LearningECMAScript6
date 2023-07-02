4e248ffe6353061996d22d4919eeb47051bbad79
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:47:16 -0400

    Template strings

---
+++ ch02/020_template_strings.js
@@ -0,0 +1,14 @@
+let str1 = `hello!!!` ;
+let str2 = "hello!!!" ;
+
+console.log(str1 === str2);
+
+// Expressions
+
+let a = 20 ;
+let b = 10 ;
+let c = "JavaScript" ;
+console.log(`My age is ${a+b} and I love ${c}`);
+
+// true
+// My age is 30 and I love JavaScript
