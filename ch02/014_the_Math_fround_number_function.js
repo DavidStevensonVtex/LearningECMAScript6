02d8270700cae30473f42c44bc02e4bfdc71aaa3
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:21:46 -0400

    The Math.fround(number) function

---
+++ ch02/014_the_Math_fround_number_function.js
@@ -0,0 +1,11 @@
+// The Math.fround function rounds a number to a 32-bit floating point value.
+
+console.log("Math.fround(0)     ", Math.fround(0));
+console.log("Math.fround(1)     ", Math.fround(1));
+console.log("Math.fround(1.137) ", Math.fround(1.137));
+console.log("Math.fround(1.5)   ", Math.fround(1.5));
+
+// Math.fround(0)      0
+// Math.fround(1)      1
+// Math.fround(1.137)  1.1369999647140503
+// Math.fround(1.5)    1.5
