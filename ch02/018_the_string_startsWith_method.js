d2dcd52b08f0fd34fece48353a570cebd8d4968b
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 14:38:55 -0400

    The String startsWith(string) and startswith(string,index) methods

---
+++ ch02/018_the_string_startsWith_method.js
@@ -0,0 +1,4 @@
+let str = "Hi, I am a JS Developer" ;
+console.log(str.startsWith('Hi, I am'));                // Output "true"
+
+console.log(str.startsWith('JS Developer', 11));        // Output "true"
