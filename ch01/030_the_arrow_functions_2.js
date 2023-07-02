1b9b72f73d52fdcdc8dc5d026bfd58b110579dc9
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 12:32:20 -0400

    The value of "this" in an arrow function

--- ch01/030_the_arrow_functions_2.js
+++ ch01/030_the_arrow_functions_2.js
@@ -4,3 +4,6 @@ let result = circleArea(3.14, 3) ;
 
 console.log(`Area of circle with radius of: 3: ${result.toFixed(2)}`);
 // Area of circle with radius of: 3: 28.26
+
+// When curly braces are not used, the expression is interpreted as a return value.
+// Curly braces may be used for multiple statements, using the return statement to return a value.
