fb311d85fb09b1787a39eaa400b3cd0d87a2f0a6
David Stevenson <drstevenson1958@yahoo.com>
2023-07-02 13:55:37 -0400

    Trigonometry functions

---
+++ ch02/008_trigonometry_functions.js
@@ -0,0 +1,16 @@
+console.log("Math.sinh(0)", Math.sinh(0));      // hyperbolic sine of a value
+console.log("Math.cosh(0)", Math.cosh(0));      // hyperbolic cosine of a value
+console.log("Math.tanh(0))", Math.tanh(0));     // hyperbolic tangent of a value
+console.log("Math.asinh(0)", Math.asinh(0)) ;   // inverse hyperbolic sine of a value
+console.log("Math.acosh(1)", Math.acosh(1));    // inverse hyperbolic cosine of a value
+console.log("Math.atanh(0)", Math.atanh(0));    // inverse hyperbolic tangent of a value
+console.log("Math.hypot(2, 2, 1)", Math.hypot(2, 2, 1));    // Pythagoras theorem
+
+
+// Math.sinh(0) 0
+// Math.cosh(0) 1
+// Math.tanh(0)) 0
+// Math.asinh(0) 0
+// Math.acosh(1) 0
+// Math.atanh(0) 0
+// Math.hypot(2, 2, 1) 3
