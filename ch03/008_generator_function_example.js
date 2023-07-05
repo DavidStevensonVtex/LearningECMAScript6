99ae9db01469776bcab251f4aa26555b2d01d0c1
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 17:34:48 -0400

    Generator function example

---
+++ ch03/008_generator_function_example.js
@@ -0,0 +1,21 @@
+function *generator_function()
+{
+    var a = yield 12 ;
+    var b = yield a + 1 ;
+    var c = yield b + 2 ;
+    yield c + 3 ;
+}
+
+var generator = generator_function() ;
+
+console.log(generator.next().value) ;
+console.log(generator.next(5).value) ;
+console.log(generator.next(11).value) ;
+console.log(generator.next(78).value) ;
+console.log(generator.next().done);
+
+// 12
+// 6
+// 13
+// 81
+// true
