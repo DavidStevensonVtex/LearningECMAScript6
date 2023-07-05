9767d2095cc95a50aa2235a9fc893923652fea3f
David Stevenson <drstevenson1958@yahoo.com>
2023-07-05 12:10:29 -0400

    Raw string examples

---
+++ ch02/025_html_as_raw_string.js
@@ -0,0 +1,13 @@
+// Some formatters will format this literal's content as HTML
+const doc =  `<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+  </body>
+</html>
+`;
+
+console.log(doc);
