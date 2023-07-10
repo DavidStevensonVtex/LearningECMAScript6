e48ceb0b4dd1b1bda21067c6328c27a5b1f031b4
David Stevenson <drstevenson1958@yahoo.com>
2023-07-10 09:38:23 -0400

    The Reflect.defineProperty ( object, property, descriptor ) method

---
+++ ch05/003_reflect_defineproperty_method.js
@@ -0,0 +1,15 @@
+const object1 = {};
+
+if (Reflect.defineProperty(object1, 'property1', { value: 42 })) {
+  console.log('property1 created!');
+  // Expected output: "property1 created!"
+} else {
+  console.log('problem creating property1');
+}
+
+console.log(object1.property1);
+// Expected output: 42
+
+const obj = {};
+Reflect.defineProperty(obj, "x", { value: 7 }); // true
+console.log(obj.x); // 7
