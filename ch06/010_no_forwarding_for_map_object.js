0465055e5ed36a6fd9c113d70ab127c72d6bce23
David Stevenson <drstevenson1958@yahoo.com>
2023-07-12 08:23:00 -0400

    No trivial forwarding proxy for Map object

---
+++ ch06/010_no_forwarding_for_map_object.js
@@ -0,0 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+
+// Some native JavaScript objects have properties called internal slots, which are not 
+// accessible from JavaScript code. For example, Map objects have an internal slot called 
+// [[MapData]], which stores the key-value pairs of the map. As such, you cannot trivially 
+// create a forwarding proxy for a map:
+
+const proxy = new Proxy(new Map(), {});
+console.log(proxy.size); // TypeError: get size method called on incompatible Proxy
