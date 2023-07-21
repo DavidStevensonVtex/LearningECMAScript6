b98cfadafaa41fdd6e6e23501ac7261f69d0e322
David Stevenson <drstevenson1958@yahoo.com>
2023-07-21 17:13:19 -0400

    A Class is a Function

---
+++ ch07/010_a_class_is_a_function.js
@@ -0,0 +1,18 @@
+class Student
+{
+    constructor (name) 
+    {
+        this.name = name ;
+    }
+}
+
+function School(name) 
+{
+    thisname = name ;
+}
+
+console.log("typeof Student", typeof Student, "typeof School", typeof School);
+console.log("typeof School == typeof Student", typeof School == typeof Student);
+
+// typeof Student function typeof School function
+// typeof School == typeof Student true
